fa3915cef304aeba8963c030393b75d570a5c14f
Kenny <kennethpolyak@gmail.com>
2018-11-12 08:24:01 -0500

    Promisify listings array generation, in case items are being skipped at scale

--- server/models/loadData.js
+++ server/models/loadData.js
@@ -1,70 +1,39 @@
 const async = require('async');
 const models = require('./models.js');
-// const generatedListings = require('./dummyData/generateListingsData.js');
+const faker = require('faker');
+var { fork } = require('child_process');
 const generatedLandmarks = require('./dummyData/generateLandmarksData.js');
+const neighbs = require('./dummyData/neighbsData.js').neighbsArray;
+// let listingsData = require('./listingsData_no_coords').listingsArray;
 const Listing = models.listingSchema;
 const Neighborhood = models.neighborhoodSchema;
 const Landmark = models.landmarkSchema;
-
-// Import arrays of data
-
-// const listings = generatedListings.listingsData;
-// const scaleListingsArrays = generatedListings.scaleListingsArray;
-const neighbs = require('./dummyData/neighbsData.js').neighbsArray;
 const landmarks = generatedLandmarks.landmarksData;
 
-// GENERATE LISTINGS DATA
-
-///////////////////// LISTINGS DATA ////////////////////////////
-//// Generate 10000000 points within a polygon of Central London ////
-
-const randomPointsOnPolygon = require('random-points-on-polygon');
-const turf = require('turf');
-
-var polygon = turf.polygon([[
-  [-.2451, 51.5194], [-.1960, 51.5485],
-  [-.1040, 51.5485], [-.0700, 51.5331],
-  [-.0632, 51.5132], [-.0748, 51.5089],
-  [-.1095, 51.5128], [-.1218, 51.5108],
-  [-.1281, 51.4972], [-.1428, 51.4867],
-  [-.1665, 51.4869], [-.1850, 51.4811],
-  [-.2451, 51.5194]
-]]);
-
-// let numberOfPoints = 10000000;
-// var points = randomPointsOnPolygon(numberOfPoints, polygon);
-
-let listingsCoords = [];
-
-// for (let i = 0; i < points.length; i++) {
-//   // reverse order to lat-long instead long-lat
-//   let latLong = [points[i].geometry.coordinates[1], points[i].geometry.coordinates[0]]
-//   listingsCoords.push(latLong)
-// }
-
-////// Add the points onto the listingsData from Mockaroo //////
-
-// let listingsData = require('./listingsData_no_coords').listingsArray;
-// let { scaleListingsArray } = require('./scaleData.js');
-
-const faker = require('faker');
-var { fork } = require('child_process');
+var totalAdded = 0;
 
 function insertAsync(callback) {
-  var totalAdded = 0;
-  function generate() {
-    var scaleListingsArray = [];
-    for (var j = 0; j < 200000; j++) {
-      var listing = {
-        "hostFirstName": faker.name.firstName(),
-        "neighbId": Math.floor(Math.random() * 10) + 1,
-        "neighbDesc": faker.lorem.sentence(),
-        // "listingLat": listingsCoords[j][0],
-        // "listingLong": listingsCoords[j][1]
+
+  function generateDummyArray() {
+    return new Promise((resolve, reject) => {
+      var scaleListingsArray = [];
+      for (var j = 0; j < 200000; j++) {
+        var listing = {
+          "hostFirstName": faker.name.firstName(),
+          "neighbId": Math.floor(Math.random() * 15) + 1,
+          "neighbDesc": faker.lorem.sentence(),
+          "gettingAroundDesc": faker.lorem.sentence()
+          // "listingLat": listingsCoords[j][0],
+          // "listingLong": listingsCoords[j][1]
+        }
+        scaleListingsArray.push(listing)
       }
-      scaleListingsArray.push(listing)
-    }
-    return scaleListingsArray;
+      if (scaleListingsArray.length !== 200000) {
+        reject();
+      } else {
+        resolve(scaleListingsArray);
+      }
+    })
   }
     
   function insert(array) {
@@ -84,7 +53,7 @@ function insertAsync(callback) {
 
   async function initialize() {
     for (var i = 0; i < 2; i++) {
-      var listingArray = generate()
+      var listingArray = await generateDummyArray()
       await insert(listingArray);
     }
     callback();
@@ -92,17 +61,16 @@ function insertAsync(callback) {
   initialize(); 
 }
 
+// Sequelize models insertion:
 Listing.sync({force: true})
 .then(() => {
   insertAsync(() => {
-    console.log('Listing table successfully populated');
+    console.log('Listing table populated with ' + totalAdded + ' records');
   });
 })
 .catch((err) => {
   console.error(err);
 })
-
-console.log('after or before?');
 
 Neighborhood.sync({force: true})
 .then(() => {
@@ -121,23 +89,3 @@ landmarks.then((results) => {
     console.error(err);
   })
 })
-
-
-
-// function insertSync() {
-//   console.log(process.memoryUsage().heapUsed/1000000);
-//   var scaleListingsArray = [];
-//   for (var j = 0; j < 200000; j++) {
-//     var listing = {
-//       "hostFirstName": faker.name.firstName(),
-//       "neighbId": Math.floor(Math.random() * 10) + 1,
-//       "neighbDesc": faker.lorem.sentence(),
-//       "listingLat": listingsCoords[j][0],
-//       "listingLong": listingsCoords[j][1]
-//     }
-//     scaleListingsArray.push(listing)
-
-//   }
-//   Listing.bulkCreate(scaleListingsArray).then(() => {console.log('Added 200,000')})  
-//   return;
-// }
